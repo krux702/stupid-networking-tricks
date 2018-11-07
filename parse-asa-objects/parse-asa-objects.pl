f0cc55cdb451a10826c5d9f69521a808840e1440
Krux <krux@thcnet.net>
2018-11-06 16:18:05 -0800

    Updated variable name and cleanded up data dumper output

--- parse-asa-objects/parse-asa-objects.pl
+++ parse-asa-objects/parse-asa-objects.pl
@@ -24,7 +24,7 @@ SYNTAX:
 
 
 
-%item = ();
+%asa_objects = ();
 $current_object = "";
 
 open (FILE, "<$filename");
@@ -34,41 +34,43 @@ while(<FILE>)
   if($_ =~ /^object-group (network|service) ([a-zA-Z0-9\._-]*)/)
   {
     $current_object = $2;
-    $item{$current_object}{'parents'} = [];
-    $item{$current_object}{'children'} = [];
+    $asa_objects{$current_object}{'parents'} = [];
+    $asa_objects{$current_object}{'children'} = [];
   }
   if($_ =~ /^ group-object (.*)$/)
   {
-     push @{ $item{$1}{'parents'}}, $current_object;
-     push @{ $item{$current_object}{'children'}}, $1;
+     push @{ $asa_objects{$1}{'parents'}}, $current_object;
+     push @{ $asa_objects{$current_object}{'children'}}, $1;
   }
 
   if($_ =~ /^access-list (.*) extended .*/ )
   {
     $current_object = $1;
-    $item{$current_object}{'parents'} = ["ACL"];
-    $item{$current_object}{'children'} = [];
+    $asa_objects{$current_object}{'parents'} = ["ACL"];
+    $asa_objects{$current_object}{'children'} = [];
 
     $acl = $_;
     while($acl =~ s/(object-group) ([^\ ]*)//)
     {
-      push @{ $item{$2}{'parents'}}, $current_object;
-      push @{ $item{$current_object}{'children'}}, $2;
+      push @{ $asa_objects{$2}{'parents'}}, $current_object;
+      push @{ $asa_objects{$current_object}{'children'}}, $2;
     }
   }
 }
 
 close(FILE);
 
-print Dumper(%item);
+# print Dumper(\%item);
+print Data::Dumper->Dump( [ \%asa_objects ], [ qw(*asa_objects) ] );
+
 
 print "\n------------\n";
 print "Unreferenced\n";
 print "------------\n";
 
-foreach $key (keys %item)
+foreach $key (keys %asa_objects)
 {
-  $parents = $#{$item{$key}{'parents'}};
+  $parents = $#{$asa_objects{$key}{'parents'}};
   if($parents eq -1)
   {
     print "$key\n";
