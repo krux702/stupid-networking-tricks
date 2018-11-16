84d45dbbd609cc4a19f47fe2ce58d0de8be84bc0
Krux <krux@thcnet.net>
2018-11-16 10:15:52 -0800

    Added expanding of ACLs

--- parse-asa-objects/parse-asa-objects.pl
+++ parse-asa-objects/parse-asa-objects.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
 
+use Term::ANSIColor qw(:constants);
 use Data::Dumper;
 use Getopt::Long;
 
@@ -66,34 +67,57 @@ while(<FILE>)
   }
   if($_ =~ /^ group-object (.*)$/)
   {
-     push @{ $asa_objects{"OBJECT-GROUP: $1"}{'parents'}}, $current_object;
+     push_uniq("OBJECT-GROUP: $1", 'parents', $current_object);
      push @{ $asa_objects{$current_object}{'children'}}, "OBJECT-GROUP: $1";
   }
   if($_ =~ /^ network-object object (.*)$/)
   {
-     push @{ $asa_objects{"OBJECT: $1"}{'parents'}}, $current_object;
+     push_uniq("OBJECT: $1", 'parents', $current_object);
      push @{ $asa_objects{$current_object}{'children'}}, "OBJECT: $1";
   }
+  elsif($_ =~ /^ network-object (.*)$/)
+  {
+    push @{ $asa_objects{$current_object}{'items'}}, $1;
+  }
+
+  if($_ =~ /^ (subnet|host) (.*)$/)
+  {
+    push @{ $asa_objects{$current_object}{'items'}}, "$1 $2";
+  }
+
   if($_ =~ /^ service-object object (.*)$/)
   {
-     push @{ $asa_objects{"OBJECT: $1"}{'parents'}}, $current_object;
+     push_uniq("OBJECT: $1", 'parents', $current_object);
      push @{ $asa_objects{$current_object}{'children'}}, "OBJECT: $1";
   }
+  elsif($_ =~ /^ service-object (.*)$/)
+  {
+    push @{ $asa_objects{$current_object}{'items'}}, $1;
+  }
 
-  if($_ =~ /^access-list (.*) extended .*/ )
+  if($_ =~ /^ fqdn v4 (.*)$/)
+  {
+    push @{ $asa_objects{$current_object}{'items'}}, "fqdn v4 $1";
+  }
+
+
+  if($_ =~ /^access-list (.*) (standard|extended) (.*)/ )
   {
     $current_object = "ACL: $1";
-    $asa_objects{$current_object}{'children'} = [];
+    $acl = "$2 $3";
+    $acl =~ s/\s+$//;
 
-    $acl = $_;
+    $asa_objects{$current_object}{'children'} = [];
+    push @{ $asa_objects{$current_object}{'items'} }, $acl;
+
     while($acl =~ s/(object-group) ([^\ ]*)//)
     {
-      push @{ $asa_objects{"OBJECT-GROUP: $2"}{'parents'} }, $current_object;
+      push_uniq("OBJECT-GROUP: $2", 'parents', $current_object);
       push @{ $asa_objects{$current_object}{'children'} }, "OBJECT-GROUP: $2";
     }
     while($acl =~ s/(object) ([^\ ]*)//)
     {
-      push @{ $asa_objects{"OBJECT: $2"}{'parents'} }, $current_object;
+      push_uniq("OBJECT-GROUP: $2", 'parents', $current_object);
       push @{ $asa_objects{$current_object}{'children'} }, "OBJECT: $2";
     }
   }
@@ -103,7 +127,7 @@ while(<FILE>)
   {
     $acl = "ACL: $1";
     $access_group = "ACCESS-GROUP: $3 $2";
-    push @{ $asa_objects{$acl}{'parents'} }, $access_group;
+    push_uniq($acl, 'parents', $access_group);
     $asa_objects{$access_group}{'parents'} = ["ACCESS_GROUPS"];
     push @{ $asa_objects{$access_group}{'children'} }, $acl;
     push @{ $asa_objects{"ACCESS_GROUPS"}{'children'} }, $access_group;
@@ -116,7 +140,7 @@ while(<FILE>)
   {
     $crypto_map = "CRYPTO_MAP: $1";
     $acl = "ACL: $2";
-    push @{ $asa_objects{$acl}{'parents'} }, $crypto_map;
+    push_uniq($acl, 'parents', $crypto_map);
     $asa_objects{$crypto_map}{'parents'} = ["CRYPTO_MAPS"];
     push @{ $asa_objects{$crypto_map}{'children'} }, $acl;
     push @{ $asa_objects{"CRYPTO_MAPS"}{'children'} }, $crypto_map;
@@ -132,28 +156,62 @@ while(<FILE>)
       if( find_object($1) ne "" )
       {
         push @{ $asa_objects{$nat_rule}{'children'} }, find_object($1);
-        push @{ $asa_objects{find_object($1)}{'parents'} }, $nat_rule;
+        push_uniq(find_object($1), 'parents', $nat_rule);
         $asa_objects{$nat_rule}{'parents'} = ["NAT_RULES"];
-        push @{ $asa_objects{"NAT_RULES"}{'children'} }, $nat_rule;
+        push_uniq("NAT_RULES", 'children', $nat_rule);
       }
     }
   }
+
+  # group-policy <group_policy_name> internal
+  # group-policy <group_policy_name> attributes
+  #  vpn-filter value <acl name>
+  #
+  #  vpn-group-policy <group_policy_name>
+  #  default-group-policy <group_policy_name>
+
+  # access-list <name> standard permit host <ip>
+
+
 }
 
 close(FILE);
 
-print "\n---------------\n";
+print RED,BOLD,"\n---------------\n";
 print "Dump of objects\n";
-print "---------------\n";
+print "---------------\n",RESET;
 
 print Data::Dumper->Dump( [ \%asa_objects ], [ qw(*asa_objects) ] );
 
 
-print "\n--------------------\n";
-print "Unreferenced objects\n";
-print "--------------------\n";
+if($expand_acls)
+{
+  print RED,BOLD,"\n-------------\n";
+  print "Expanded ACLs\n";
+  print "-------------\n",RESET;
 
-foreach $key (keys %asa_objects)
+  foreach $key (sort keys %asa_objects)
+  {
+    if($key =~ /^ACL: /)
+    {
+      print BOLD, BLUE, "$key\n", RESET;
+      foreach $acl (@{$asa_objects{$key}{'items'}})
+      {
+        print BOLD, WHITE, "$acl\n", RESET;
+        expand_acl($acl);
+
+      }
+      print "\n";
+    }
+  }
+}
+
+
+print RED,BOLD,"\n--------------------\n";
+print "Unreferenced objects\n";
+print "--------------------\n",RESET;
+
+foreach $key (sort keys %asa_objects)
 {
   $parents = $#{$asa_objects{$key}{'parents'}};
   if($parents eq -1)
@@ -161,6 +219,8 @@ foreach $key (keys %asa_objects)
     print "$key\n";
   }
 }
+
+
 
 sub find_object
 {
@@ -173,5 +233,171 @@ sub find_object
   if(defined $asa_objects{"OBJECT-GROUP: $object"})
   {
     return "OBJECT-GROUP: $object";
+  }
+}
+
+sub expand_acl
+{
+  my ($acl) = @_;
+
+  @expanded_acl = ("");
+  $acl_work = "";
+
+  while($acl)
+  {
+    if($acl =~ s/^object ([^\s]+)//)
+    {
+      $object = "OBJECT: $1";
+
+      foreach $index (keys @expanded_acl)
+      {
+        $expanded_acl[$index] .= $acl_work;
+      }
+      $acl_work = "";
+
+      if(defined $asa_objects{$object})
+      {
+        @objects = expand_object($object);
+        @new_expanded_acl = ();
+        foreach $index (keys @expanded_acl)
+        {
+          foreach $item (@objects)
+          {
+            push @new_expanded_acl, $expanded_acl[$index] . $item;
+          }
+        }
+        @expanded_acl = @new_expanded_acl;
+      }
+    }
+
+    if($acl =~ s/^object-group ([^\s]+)//)
+    {
+      $object = "OBJECT-GROUP: $1";
+
+      foreach $index (keys @expanded_acl)
+      {
+        $expanded_acl[$index] .= $acl_work;
+      }
+      $acl_work = "";
+
+      if(defined $asa_objects{$object})
+      {
+        @objects = expand_object($object);
+        @new_expanded_acl = ();
+        foreach $index (keys @expanded_acl)
+        {
+          foreach $item (@objects)
+          {
+            push @new_expanded_acl, $expanded_acl[$index] . $item;
+          }
+        }
+        @expanded_acl = @new_expanded_acl;
+      }
+    }
+
+    if($acl =~ s/^(\s+)//)
+    {
+      $acl_work .= $1;
+    }
+    elsif($acl =~ s/^(extended|standard)//)
+    {
+      $acl_work .= $1;
+    }
+    elsif($acl =~ s/^(permit|deny)//)
+    {
+      $acl_work .= $1;
+    }
+    elsif($acl =~ s/^(ip|tcp|udp|icmp|esp|ah)//)
+    {
+      $acl_work .= $1;
+    }
+    elsif($acl =~ s/^(host [0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)//)
+    {
+      $acl_work .= $1;
+    }
+    elsif($acl =~ s/^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+ [0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)//)
+    {
+      $acl_work .= $1;
+    }
+    elsif($acl =~ s/^(eq [^\s]+)//)
+    {
+      $acl_work .= $1;
+    }
+    elsif($acl =~ s/^(interface [^\s]+)//)
+    {
+      $acl_work .= $1;
+    }
+    elsif($acl =~ s/^(any4|any6|any)//)
+    {
+      $acl_work .= $1;
+    }
+    elsif($acl =~ s/^(range [0-9]+ [0-9]+)//)
+    {
+      $acl_work .= $1;
+    }
+    elsif($acl =~ s/^(time-range [^\s]+)//)
+    {
+      $acl_work .= $1;
+    }
+    elsif($acl =~ s/^(log)//)
+    {
+      $acl_work .= $1;
+    }
+    elsif($acl =~ s/^(inactive)//)
+    {
+      $acl_work .= $1;
+    }
+    elsif($acl =~ s/^([^\s]+)//)
+    {
+      $acl_work .= $1;
+    }
+
+  }
+
+  foreach $index (keys @expanded_acl)
+  {
+    $expanded_acl[$index] .= $acl_work;
+  }
+  $acl_work = "";
+
+  foreach $line (@expanded_acl)
+  {
+    $line =~ s/\ \ /\ /g;
+    print " $line\n";
+  }
+  
+}
+
+sub expand_object
+{
+  my ($object) = @_;
+  my @object_array = ();
+
+  foreach $item (@{$asa_objects{$object}{'items'}})
+  {
+    push @object_array, $item;
+  }
+  foreach $child_object (@{$asa_objects{$object}{'children'}})
+  {
+    push @object_array, expand_object($child_object);
+  }
+  return @object_array;
+}
+
+sub push_uniq
+{
+  my ($object, $reference, $item) = @_;
+
+  my $found = 0;
+  foreach $test (@{$asa_objects{$object}{$reference}})
+  {
+    if($test eq $item)
+    {
+      $found = 1;
+    }
+  }
+  if(! $found)
+  {
+    push @{ $asa_objects{$object}{$reference} }, $item;
   }
 }
