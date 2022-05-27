6ce861d1bc40fb4efc1c51876260ed2f8335b50e
Krux <{ID}+{username}@users.noreply.github.com>
2022-05-27 15:53:33 -0700

    Added split stream utility to extract data from PCAP files

---
+++ split_stream/README.md
@@ -0,0 +1,6 @@
+# Split Stream
+
+`split_stream` is a tool that reads in a packet capture file and breaks out
+the individual TCP and UDP data streams for further analysis. I wrote this
+for use in CTF challenges.
+
---
+++ split_stream/split_stream.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if [ "$1" == "" ] ; then
+  cat <<END
+Split stream - splits a packet capture into it's individual TCP and UDP streams for analysis.
+
+syntax: ./split_stream.sh <pcap_file> [output_dir]
+
+END
+  exit
+fi
+
+PCAP="$1"
+OUTPUT_DIR=${PCAP}_streams
+OUTPUT_DIR=${2:-$OUTPUT_DIR}
+
+if [[ ! -d $OUTPUT_DIR && -f $OUTPUT_DIR ]] ; then
+  echo "Error: $OUTPUT_DIR must not be a file"
+  exit
+else
+  mkdir -p $OUTPUT_DIR
+fi
+
+FILE_TYPE="`file ${PCAP} | grep 'capture file'`"
+if [ "$FILE_TYPE" = "" ] ; then
+  echo "Error: $PCAP is not a pcap file"
+  file $PCAP
+  exit
+fi
+
+TCPSTR=`tshark -nr ${PCAP} -q -z conv,tcp | grep -c "<->"`
+TCPSESS="$(($TCPSTR-1))"
+
+for i in $(seq 0 $TCPSESS); do
+  echo extracting TCP stream $i
+  tshark -r ${PCAP} -z follow,tcp,raw,$i | \
+    grep -E "^(\s+|)[0-9a-f]*$" | \
+    tr -d '=\r\n\t' | xxd -r -p > $OUTPUT_DIR/session_tcp_$i.bin
+done
+
+UDPSTR=`tshark -nr ${PCAP} -q -z conv,udp | grep -c "<->"`
+UDPSESS="$(($UDPSTR-1))"
+
+for i in $(seq 0 $UDPSESS); do
+  echo extracting UDP stream $i
+  tshark -r ${PCAP} -z follow,udp,raw,$i | \
+    grep -E "^(\s+|)[0-9a-f]*$" | \
+    tr -d '=\r\n\t' | xxd -r -p > $OUTPUT_DIR/session_udp_$i.bin
+done
+
+echo
+echo "$OUTPUT_DIR/"
+ls -l $OUTPUT_DIR/
